e626f5c78d24342285d0a420f47d81b2d7857984
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-10-02 16:41:42 +0530

    modules

--- outputs.tf
+++ outputs.tf
@@ -6,14 +6,18 @@ output "ig_id" {
   value = aws_internet_gateway.main.id
 }
 
-output "public_subnet_ids" {
+output "public_subnet_ids"{
   value = aws_subnet.public[*].id
 }
 
-output "private_subnet_ids" {
+output "private_subnet_ids"{
   value = aws_subnet.private[*].id
 }
 
-output "database_subnet_ids" {
+output "database_subnet_ids"{
   value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_name"{
+  value = aws_db_subnet_group.default.name
 }
