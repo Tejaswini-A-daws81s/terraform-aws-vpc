322a2ea804ec73ea246a0e4b3644f54ef9c05a77
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-11-04 05:58:48 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,6 @@ output "database_subnet_ids"{
   value = aws_subnet.database[*].id
 }
 
-output "database_subnet_group_name"{
-  value = aws_db_subnet_group.default.name
-}
+# output "database_subnet_group_name"{
+#   value = aws_db_subnet_group.default.name
+# }
